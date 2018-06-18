d9e766eb88c0e7e8883a8cdb3e69aafd0f79c7b7
Samuel-Hall <samuel.r.hall14@gmail.com>
2018-06-18 19:54:51 -0400

    fully functional, needs style and content

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,18 @@
+// Fill in the actual questions
+// Finish styling the page
+// Write the README
+// Push to pages
+// Add to portfolio page
+// Submit links to BCS
+
+
 $(document).ready(function() {
     // Create variables to hold number of right, wrong, and unanswered questions.
     var questionsCorrect = 0;
     var questionsIncorrect = 0;
-    var questionsNotAnswered = 0;
-    var wins = 0;
-    var losses = 0;
+    var questionsAnswered = 0;
+    // Note, moved questionsNotAnswered below the questions array to correct undefined error
+
     // Create an array. Each element in the array is an object containing a question and several possible answers. Only one of the answers is the correct answer. 
     
     var questionOne = {
@@ -43,7 +51,10 @@ $(document).ready(function() {
         incorrectAnswerThree: "This is 5 incorrect three"
     }
     
-    var questions = [questionOne, questionTwo, questionThree, questionFour, questionFive]
+    var questions = [questionOne, questionTwo, questionThree, questionFour, questionFive];
+    var questionIndex = 0;
+    var currentQuestion = questions[questionIndex];    
+    var questionsNotAnswered = questions.length;
     // Create variable to hold SKATE before and after the letters have been collected. 
     var skate = ["S", "K", "A", "T", "E"];
     var skateLettersEarned = [];
@@ -66,88 +77,118 @@ $(document).ready(function() {
         return array;
     } 
     
-    $("#start").on("click", startGame);
-
-    var intervalId;
-    // prevents the clock from being sped up unnecessarily
-    var clockRunning = false;
+    $(document).on("click", ".start", startGame);   
     
-    // Create a 2:00 timer
-   
-    var time = 120;
-
-    function reset() {
-        time = 120;
-        $("#timer").text("2:00");
-    }
-
-    function startTimer() {
-        intervalId = setInterval(count, 1000);
-        clockRunning = true;
-    }
-
-    function stopTimer() {
-        clearInterval(intervalId);
-        clockRunning = false;
-    }
-
-    function count() {
-        time--;
-        if (time == 0) {
-            stopTimer();
-            reset();
-        }
-        var converted = timeConverter(time);
-        $("#timer").text(converted);
-
-    }
-
-    function timeConverter(t) {
-
-        var minutes = Math.floor(t / 60);
-        var seconds = t - (minutes * 60);
-
-        if (seconds < 10) {
-            seconds = "0" + seconds;
-        }
-        
-        return minutes + ":" + seconds;
-    }
-
     // Game logic
     var questionIndex = 0;
     var currentQuestion = questions[questionIndex]
-
+    
     function startGame() {
+        // Note, to fix scoping issue and allow the timeout function to be a part of the game function to allow endGame at time==0, moving all timer related functions inside the startGame function
+        var intervalId;
+        // prevents the clock from being sped up unnecessarily
+        var clockRunning = false;
+        
+        // Create a 2:00 timer
+       
+        var time = 120;
+    
+        function reset() {
+            time = 120;
+            questionsNotAnswered = questions.length;
+            $("#timer").text("2:00");
+            $("#skate").text("");
+            $("#hiddenTape").html("");
+            $("#collectSkate").text("Collect the letters of SKATE");
+            $("#findTape").text("Find the hidden tape");
+            $("#notAnswered").text(questionsNotAnswered);
+            skateIndex = 0;
+            skateLettersEarned = [];
+            questionsCorrect = 0;
+            questionsIncorrect = 0;
+            questionsAnswered = 0;
+            questionIndex = 0;
+        }
+    
+        function startTimer() {
+            intervalId = setInterval(count, 1000);
+            clockRunning = true;
+        }
+    
+        function stopTimer() {
+            clearInterval(intervalId);
+            clockRunning = false;
+        }
+    
+        function count() {
+            time--;
+            if (time == 0) {
+                endGame();
+            }
+            var converted = timeConverter(time);
+            $("#timer").text(converted);
+    
+        }
+    
+        function timeConverter(t) {
+    
+            var minutes = Math.floor(t / 60);
+            var seconds = t - (minutes * 60);
+    
+            if (seconds < 10) {
+                seconds = "0" + seconds;
+            }
+            
+            return minutes + ":" + seconds;
+        }
+
+        reset();
         // put questions in a random order
         shuffleArray(questions);
-        
-
         startTimer();
-        showQuestion();
-        
+        showQuestion();        
 
         function showQuestion() {
+            $("#notAnswered").text(questionsNotAnswered);
+
             // reassign the current question
-            currentQuestion = questions[questionIndex]
+            currentQuestion = questions[questionIndex];
+
+            // take answers and randomize the order
+            // Object.values???
+            var randomAnswers = Object.keys(currentQuestion);
+            randomAnswers.splice(0, 1);
+            shuffleArray(randomAnswers);
+
             // restart click event listener
             $(document).off("click");
 
-            // // Pick a random question
-            // var currentQuestion = questions[Math.floor(0 + Math.random() * questions.length)];
-            // Create a form
+            // Create a form to hold the questions and answers
             $("#quizArea").html("<form id='form'></form>");
             // Append question to form
             $("#form").append("<h2>" + currentQuestion.question + "</h2>");
-            $("#form").append("<input type='button' class='btn btn-secondary btn-block answer' value='" + currentQuestion.correctAnswer + "'></input><br>");
-            $("#form").append("<input type='button' class='btn btn-secondary btn-block answer' value='" + currentQuestion.incorrectAnswerOne + "'></input><br>");
-            $("#form").append("<input type='button' class='btn btn-secondary btn-block answer' value='" + currentQuestion.incorrectAnswerTwo + "'></input><br>");
-            $("#form").append("<input type='button' class='btn btn-secondary btn-block answer' value='" + currentQuestion.incorrectAnswerThree + "'></input><br>");
+
+            // I'm thinking I can sort the questions into random order and then run this as a for loop
+            // currentQuestion is questions[i];
+            // var array = Object.keys(currentQuesion); console log question + answers
+            // array.splice(0, 1);
+            // //Now array = [just the answers];
+            // shuffleArray(array);
+            // for loop to print the quesion buttons
+
+            // Attempting the for loop
+            for (var k = 0; k < randomAnswers.length; k++) {
+                $("#form").append("<input type='button' class='btn btn-secondary btn-block answer' value='" + currentQuestion[randomAnswers[k]] + "'></input><br>");
+            }
+
+            // $("#form").append("<input type='button' class='btn btn-secondary btn-block answer' value='" + currentQuestion.correctAnswer + "'></input><br>");
+            // $("#form").append("<input type='button' class='btn btn-secondary btn-block answer' value='" + currentQuestion.incorrectAnswerOne + "'></input><br>");
+            // $("#form").append("<input type='button' class='btn btn-secondary btn-block answer' value='" + currentQuestion.incorrectAnswerTwo + "'></input><br>");
+            // $("#form").append("<input type='button' class='btn btn-secondary btn-block answer' value='" + currentQuestion.incorrectAnswerThree + "'></input><br>");
 
             // Determine if the answer chosen is correct
             function checkAnswer() {
                 userSelect = this.value;
-                console.log(userSelect);
                 if (userSelect !== currentQuestion.correctAnswer) {
                     incorrectAnswer();
                 }
@@ -163,9 +204,17 @@ $(document).ready(function() {
                 $("#form").append("<input type='button' class='btn btn-secondary btn-block' value='" + currentQuestion.correctAnswer + "'></input><br>");
                 if (questionIndex !== (questions.length -1)){
                     questionIndex++;
-                    console.log(questionIndex);
                     setTimeout(function() {
+                        questionsAnswered++;
+                        questionsNotAnswered = questions.length - questionsAnswered;
                         showQuestion();
+                    }, 3500);
+                }
+                else {
+                    setTimeout(function() {
+                    questionsAnswered++;
+                    questionsNotAnswered = questions.length - questionsAnswered;
+                    finishQuestions();
                     }, 3500);
                 }
             }
@@ -181,11 +230,24 @@ $(document).ready(function() {
                 displaySkate();
                 if (questionIndex !== (questions.length -1)){
                     questionIndex++;
-                    console.log(questionIndex);
                     setTimeout(function() {
+                        questionsAnswered++;
+                        questionsNotAnswered = questions.length - questionsAnswered;
                         showQuestion();
                     }, 3500);
                 }
+                else {
+                    setTimeout(function() {
+                    questionsAnswered++;
+                    questionsNotAnswered = questions.length - questionsAnswered;
+                    finishQuestions();
+                    }, 3500);
+                }
+                // Find the hidden tape by answering all questions correctly
+                if (questionsCorrect == questions.length) {
+                    $("#findTape").text("You found the hidden tape!");
+                    $("#hiddenTape").append("<img  src='assets/images/hiddenTape.jpg' alt='Hidden tape' style='width:80px;height:80px;' />");
+                };
             }
 
             // Starting event listener
@@ -201,7 +263,24 @@ $(document).ready(function() {
             }
             else {
                 $("#skate").text(skateLettersEarned);
+                $("#collectSkate").text("You got SKATE!");
             };
-        };        
+        };
+
+        // End of game criteria: Time is up, or all questions are answered
+        function finishQuestions() {
+            if (questionsAnswered == questions.length) {
+                endGame();
+            }
+        };
+        
+        function endGame() {
+            stopTimer();
+            $("#notAnswered").text(questionsNotAnswered);
+            $("#quizArea").html("<h3>Right answers: " + questionsCorrect + "</h3><h3>Wrong answers: " + questionsIncorrect + "</h3>");
+            $("#quizArea").append("<button type='button' class='btn btn-lg btn-secondary start' id='start'>Play again</button>");
+            $(document).off("click");
+            $(document).on("click", ".start", startGame);
+        };
     };       
 });
